907aaa9473ac7352c4625d95585d5f25b2812747
Mikhail Bazhanov <112183386+moverzd@users.noreply.github.com>
2024-07-11 17:09:45 +0300

    Update #7
    fixed github repository

---
+++ gleencore/js/main.js
@@ -0,0 +1,50 @@
+// Toggle menu 
+
+var navLinks = document.getElementById("navLinks");
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-100%";
+}
+
+// Scroll to Facts
+
+function scrollToFacts() {
+    hideMenu();
+    setTimeout(() => {
+        const element = document.querySelector(".facts");
+        element.scrollIntoView({ behavior: "smooth" });
+    }, 100); // timming for closing
+}
+
+//Scroll to Projects 
+
+function scrollToProjects() {
+    hideMenu();
+    setTimeout(() => {
+        const element = document.querySelector(".projectsbox");
+        element.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+}
+
+//Scroll to Partners
+
+function scrollToPartners() {
+    hideMenu();
+    setTimeout(() => {
+        const element = document.querySelector(".partners");
+        element.scrollIntoView({ behavior: "smooth" });
+    }, 100); 
+}
+
+//Scroll to Contacts
+
+function scrollToContacts() {
+    hideMenu();
+    setTimeout(() => {
+        const element = document.querySelector(".contacts");
+        element.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+}
